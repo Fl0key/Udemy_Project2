25298327c3dbc73c0392677354722faa708c79f4
Seshu Rao <seshu311@gmail.com>
2023-10-12 19:09:29 +0530

    updated security group

--- terraform_code/V4-EC2-With_VPC_for_each.tf
+++ terraform_code/V4-EC2-With_VPC_for_each.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "demo-server" {
    instance_type = "t2.micro"
    key_name = "dpp"
    //security_groups = ["demo-sg"]
+   vpc_security_group_ids = [aws_security_group.demo-sg.id]
    subnet_id = aws_subnet.dpp-public-subnet-01.id
    for_each = toset(["jenkins-master","build-slave","ansible"])
    tags = {
